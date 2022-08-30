7ad18e0f6748a0f5058df6af09609be6a137a3d1
MacHD <machd@MacHDs-iMac.local>
2022-08-30 17:01:33 +0200

    2.0.0

--- index.c
+++ index.c
@@ -2,7 +2,16 @@
 
 int main ()
 {
-    printf("Hello World!\n");
+    int x =50 , y=40 , sum;
 
+    printf("Hello !\n");
+    sum = add( x , y );
+    printf("%d" , sum);
+    
     return 0;
+}
+
+int add(int a , int b)
+{
+    return a+b;
 }
